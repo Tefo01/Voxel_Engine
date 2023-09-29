9efa020eea0ede9dcab26a73102a396f4524475b
Tefo01 <ttarici0@gmail.com>
2023-09-29 16:22:09 +0300

    couldnt solve the problem with glsl compiler

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame as pg
+import moderngl
+import sys
+import pygame.time
+
+from model import *
+
+class GraphicsEngine:
+    def __init__(self, win_size=(800,400)):
+        # init pygame
+        pygame.init()
+        # window size
+        self.WIN_SIZE = win_size
+        # opengl attr
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
+        # create opengl context
+        pg.display.set_mode(self.WIN_SIZE,flags=pygame.OPENGL | pygame.DOUBLEBUF)
+        # use existing opngl context
+        self.ctx = moderngl.create_context()
+        # create an object for time tracking
+        self.cloak = pygame.time.Clock()
+        #scene
+        self.scene=Triangle(self)
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or(event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                self.scene.destroy()
+                pygame.quit()
+                sys.exit()
+    def render(self):
+        # clear framebuffer
+        self.ctx.clear(color=(0.08, 0.16, 0.18))
+        #scene
+        self.scene.render()
+        # swap buffer
+        pygame.display.flip()
+    def run(self):
+        while True:
+            self.check_events()
+            self.render()
+            self.cloak.tick(60)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__=='__main__':
+    app = GraphicsEngine()
+    app.run()
